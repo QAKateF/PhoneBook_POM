597e4e2d48ed13c7556d4d9a9c898b7f9864e0ce
QAKateF <134771804+QAKateF@users.noreply.github.com>
2023-08-11 08:30:27 +0300

    CW21

---
+++ src/test/java/LaunchTest.java
@@ -0,0 +1,12 @@
+import config.AppiumConfiguration;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import screens.SplashScreen;
+
+public class LaunchTest extends AppiumConfiguration {
+    @Test
+    public void launch(){
+        String version = new SplashScreen(driver).getCurrentVersion();
+        Assert.assertTrue(version.contains("1.0.0"));
+    }
+}
